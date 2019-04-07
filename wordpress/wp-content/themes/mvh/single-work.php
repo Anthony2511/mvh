281de7b6efb81da1efb976153dacce2a5da555be
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-04-07 12:24:47 +0200

    Added all templates

---
+++ wordpress/wp-content/themes/mvh/single-work.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Page Single Chantiers
+*/
+
+
+include( 'head.php' ); ?>
+
+    <body>
+<h1 class="hidden" aria-level="1" role="heading"><?php the_title(); ?></h1>
+<?php get_header(); ?>
+<?php include( 'navigation.php' ); ?>
+
+<div class="wrap"></div>
+
+<?php include('footer.php'); ?>

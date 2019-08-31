69497cfc1a7227c5845dd6cddd66a7c560b6b196
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-08-31 12:18:39 +0200

    Animation hover menu item

--- wordpress/wp-content/themes/mvh/navigation.php
+++ wordpress/wp-content/themes/mvh/navigation.php
@@ -22,9 +22,9 @@
             $thePostID = $post->ID; ?>
             <?php foreach (b_get_menu_items('main-nav') as $navItem): ?>
                 <li class="nav__item">
-                    <a class="nav__link" href="<?= $navItem->url; ?>"
-                       title="Vers la page <?php echo $navItem->label; ?>"><span
-                                class="nav__span <?php echo $thePostID == $navItem->id ? "nav__span--active" : ""; ?>"><?php echo $navItem->label; ?></span></a>
+                    <a class="nav__link <?php echo $thePostID == $navItem->id ? "active" : ""; ?> <?php if (is_front_page()): ?>span-home<?php else: ?><?php endif; ?>" href="<?= $navItem->url; ?>"
+                       title="Vers la page <?php echo $navItem->label; ?>">
+                        <span class="nav__span"><?php echo $navItem->label; ?></span></a>
                 </li>
             <?php endforeach; ?>
         </ul>
